7fd7440e39a7f97c3b46ab6f235f25b2ac442dc1
John Steel <jsteel-cc@conestogac.on.ca>
2014-12-14 19:41:29 -0500

    Got cookies working with SimpleCookie instead of CookieJar

--- PyByWeb.py
+++ PyByWeb.py
@@ -1,12 +1,23 @@
 from VBrowser import VBrowser
 
-httpBin = VBrowser("httpbin.org")
-response = httpBin.post("/post", params={"username": "joe", "password": "demo", "submit": "Login"})
+def printReceipt(vBrowser):
+    responseObj = httpBin.responseObjs[0];
+    responseStr = httpBin.responseStrs[0];
+    print("Request returned: %s - %s" % (responseObj.status, responseObj.reason))
+    lines = responseStr.split(b"\n")
+    for line in range(len(lines)):
+        print(lines[line])
 
-#print(response)
-lines = response.split(b"\n")
-for line in range(len(lines)):
-    print(lines[line])
+httpBin = VBrowser("httpbin.org")
+
+#httpBin.post("/post", params={"username": "joe", "password": "demo", "submit": "Login"})
+#printReceipt(httpBin)
+
+httpBin.get("/cookies/set?k1=v1&k2=v2")
+printReceipt(httpBin)
+
+httpBin.get("/cookies")
+printReceipt(httpBin)
 
 """
 username = input("Username: ")
--- VBrowser.py
+++ VBrowser.py
@@ -1,4 +1,4 @@
-import http.client, urllib.parse
+import http.client, urllib.parse, http.cookies
 
 
 class VBrowser:
@@ -6,6 +6,7 @@ class VBrowser:
 
     def __init__(self, host):
         self.host = host
+        self.cookies = http.cookies.SimpleCookie()
         self.connection = http.client.HTTPSConnection(host)
         self.headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
         self.responseObjs = []
@@ -15,9 +16,14 @@ class VBrowser:
         params = urllib.parse.urlencode(params)
         self.connection.request(method, path, params, self.headers)
         response = self.connection.getresponse()
+        headerItems = response.headers.items()
+        for i in range(len(headerItems)):
+            if "Set-Cookie" == headerItems[i][0]:
+                self.cookies.load(headerItems[i][1])
+                self.headers["Cookie"] = self.cookies.output(attrs=[], header="")
         self.responseObjs.insert(0, response)
         self.responseStrs.insert(0, response.read())
-        return self.responseStrs[0]
+        return response.status
 
     def get(self, path, params={}):
         return self.request(path, "GET", params=params)
